91f0269a72e8cc46008425ba155fc2bbf31052cc
leighthexgboy <62182180+leighthexgboy@users.noreply.github.com>
2021-06-19 12:17:31 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php 
 $target="butie2";
-$str = file_get_contents('http://47.242.208.146:3331/sender/'.$target);
+$str = file_get_contents('https://test.cheapdodo.top/sender/'.$target);
 $str = str_replace("let ip='","",$str);
 // echo($str);
 $new_url = explode("';var a",$str)[0];
